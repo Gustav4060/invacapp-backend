fb79acce27609b45395bd597fd48a759cd67b4e8
Gustav4060 <gustav4060eps@gmail.com>
2022-04-07 22:47:29 -0500

    creación proyecto
    creación proyecto

---
+++ src/main/java/com/kruger/InvacappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kruger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvacappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvacappBackendApplication.class, args);
+	}
+
+}
